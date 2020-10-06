2356324cd9191da0659ffedc02f5641398296af0
xyh <970387825@qq.com>
2020-10-06 17:07:49 +0800

    change config seting

--- config/index.js
+++ config/index.js
@@ -2,20 +2,79 @@
 // Template version: 1.3.1
 // see http://vuejs-templates.github.io/webpack for documentation.
 
-const path = require('path')
+const path = require('path');
 
+/**
+ *  author: xuyinghao
+ *  use baseUrl to api and proxyTable for server
+ *  we can use baseUrl to  change ask server, so we use virese baseUrl golbal 
+ */
+
+let baseUrl = 'http://localhost:8080/';
 module.exports = {
+  /**
+   *  build is npm init build for go to line 
+   *  for production
+   */
+  build: {
+    env: require('./prod.env'),
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    productionSourceMap: false,
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+    bundleAnalyzerReport: process.env.npm_config_report
+    
+  },
+  /**
+   *  this seting config 
+   * 
+   */
   dev: {
+    env: require("./dev.env"),
 
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+
+    /**
+     * proxyTable for server request orider
+     * web and server middleware to go
+     */
+    proxyTable: {
+      '/auth': {
+        target: baseUrl,
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: {
+          '^/auth': '/auth'
+        }
+      },
+      '/admin': {
+        target: baseUrl,
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: {
+          '^/admin': '/admin'
+        }
+      },
+      '/api': {
+        target: baseUrl,
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: {
+          '^/api': ''
+        },
+        logLevel: false
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 9090, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
+    autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
@@ -36,34 +95,4 @@ module.exports = {
     cssSourceMap: true
   },
 
-  build: {
-    // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
-
-    // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
-
-    /**
-     * Source Maps
-     */
-
-    productionSourceMap: true,
-    // https://webpack.js.org/configuration/devtool/#production
-    devtool: '#source-map',
-
-    // Gzip off by default as many popular static hosts such as
-    // Surge or Netlify already gzip all static assets for you.
-    // Before setting to `true`, make sure to:
-    // npm install --save-dev compression-webpack-plugin
-    productionGzip: false,
-    productionGzipExtensions: ['js', 'css'],
-
-    // Run the build command with an extra argument to
-    // View the bundle analyzer report after build finishes:
-    // `npm run build --report`
-    // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
-  }
 }
